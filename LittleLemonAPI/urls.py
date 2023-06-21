73543072ec858ce9104521e12380910a2988bf66
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-21 19:37:44 +0700

    api menu_items

---
+++ LittleLemonAPI/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('category', views.CategoriesView.as_view()),
+    # path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items', views.menu_items),
+    # path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/<int:pk>', views.menu_items_detail, name='menu-items-detail'),
+    path('', include('djoser.urls')),
+]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.core.paginator import Paginator, EmptyPage
+from django.contrib.auth.decorators import permission_required
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.views import APIView
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.decorators import permission_classes, throttle_classes
+from django.contrib.auth.models import User, Group
+
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+from rest_framework import generics
+from .models import Category, MenuItem, Cart, Order, OrderItem
+from .serializers import CategorySerializer, MenuItemSerializer
 
 # Create your views here.
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    throttle_classes = [AnonRateThrottle,UserRateThrottle]
+    queryset = MenuItem.objects.select_related('category').all()
+    serializer_class = MenuItemSerializer
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def menu_items(req):
+    if req.method =='GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data, status.HTTP_200_OK)
+    
+    if req.user.groups.filter(name='Manager').exists():
+        if( req.method == 'POST'):
+            serialized_item = MenuItemSerializer(data = req.data)
+            serialized_item.is_valid(raise_exception=True)
+            serialized_item.save()
+            return Response(serialized_item.data, status.HTTP_201_CREATED)
+
+    return Response({"message":"You are not authorized"}, status.HTTP_403_FORBIDDEN)
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def menu_items_detail(req, pk):
+    items = get_object_or_404(MenuItem,pk=pk)
+    serialized_item = MenuItemSerializer(items)
+    return Response(serialized_item.data, status.HTTP_200_OK) 
